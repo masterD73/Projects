204a454ce04d0ade8aa753384abe10ba315c42b0
masterD73 <105000499+masterD73@users.noreply.github.com>
2026-05-05 10:29:22 +0300

    Add files via upload

---
+++ docker/q21/names.sh
@@ -0,0 +1,16 @@
+#Reviewer: Alexander Volkovich.
+#!/bin/bash
+while true; do
+	echo "Daniel Merchav"
+	echo "Alexander Volkovich"
+	echo "Anan Yablonko"
+	echo "Shay"
+	echo "Netta"
+	echo "Sea"
+	echo "Gai"
+	echo "Tomer Gross"
+	echo "Shoval"
+	echo "Ran Ben-Moshe"
+	echo "Daniel Braunstain"
+	sleep 1
+done
